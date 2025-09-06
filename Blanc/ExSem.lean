66bbaf6f706c25303557f96474cfbc81fa4f4086
skbaek <seulkeebaek@gmail.com>
2025-09-06 14:35:17 +0900

    use arrays for blake2f

--- Blanc/ExSem.lean
+++ Blanc/ExSem.lean
@@ -2272,26 +2272,30 @@ def blake2IV : List Nat :=
     0x5BE0CD19137E2179
   ]
 
-def blake2MixTable : List (List Nat) :=
-  [
-    [0, 4, 8, 12], [1, 5, 9, 13],
-    [2, 6, 10, 14], [3, 7, 11, 15],
-    [0, 5, 10, 15], [1, 6, 11, 12],
-    [2, 7, 8, 13], [3, 4, 9, 14]
+def blake2MixTable : Array (Array Nat) :=
+  #[
+    #[0, 4, 8, 12],
+    #[1, 5, 9, 13],
+    #[2, 6, 10, 14],
+    #[3, 7, 11, 15],
+    #[0, 5, 10, 15],
+    #[1, 6, 11, 12],
+    #[2, 7, 8, 13],
+    #[3, 4, 9, 14]
   ]
 
-def blake2Sigma : List (List Nat) :=
-  [
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-    [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
-    [11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
-    [7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
-    [9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
-    [2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
-    [12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
-    [13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
-    [6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
-    [10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0]
+def blake2Sigma : Array (Array Nat) :=
+  #[
+    #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    #[14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
+    #[11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
+    #[7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
+    #[9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
+    #[2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
+    #[12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
+    #[13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
+    #[6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
+    #[10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0]
   ]
 
 /- def spit_le_to_uint -/
@@ -2370,7 +2374,7 @@ def Blake2.bCompress (b2 : Blake2) (numRounds : Nat)
       0
     ]
 
-  let innerFun (s : List Nat) (i : Nat) (v : Array Nat) : Array Nat :=
+  let innerFun (s : Array Nat) (i : Nat) (v : Array Nat) : Array Nat :=
 
     b2.g v
       ((blake2MixTable.get! i).get! 0)
@@ -2382,14 +2386,13 @@ def Blake2.bCompress (b2 : Blake2) (numRounds : Nat)
 
   let outerFun (r : Nat) (v : Array Nat) : Array Nat :=
 
-    let s : List Nat := blake2Sigma.get! (r % blake2Sigma.length)
+    let s : Array Nat := blake2Sigma.get! (r % blake2Sigma.size)
     iterRange false 8 (innerFun s) v
 
   let v := Array.toList (iterRange true numRounds outerFun ⟨v⟩)
   let resultMsgWords :=
     (List.range 8).map <| fun i => h.get! i ^^^ v.get! i ^^^ v.get! (i + 8)
   List.flatten <| resultMsgWords.map (fun n => n.toB8L.reverse.takeD 8 (0x00 : B8))
-
 
 -- blake2f
 def executeBlake2F (evm : Evm) : Execution := do
