996158940dd810fd4095c25f419621ad1ced7080
skbaek <seulkeebaek@gmail.com>
2026-07-20 01:56:49 +0900

    opt : step 4

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.23.0"
 
 require elevm from git
-  "https://github.com/skbaek/elevm.git" @ "f621e16025bc84239e11de485a69577d9ef8a4d6"
+  "https://github.com/skbaek/elevm.git" @ "4a7d293c10651390efafd31a52c3a0c5777c2aa8"
 
 @[default_target]
 lean_lib «Blanc» where
