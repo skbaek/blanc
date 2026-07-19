f7ff19a22bfd759830e72f66a8816c2f865247bd
skbaek <seulkeebaek@gmail.com>
2026-07-19 15:17:38 +0900

    add minimal CI

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.23.0"
 
 require elevm from git
-  "https://github.com/skbaek/elevm.git" @ "main"
+  "https://github.com/skbaek/elevm.git" @ "f621e16025bc84239e11de485a69577d9ef8a4d6"
 
 @[default_target]
 lean_lib «Blanc» where
