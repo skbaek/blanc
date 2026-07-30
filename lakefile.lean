41ce8f5c221738c91785b02f46669679c37bac7b
skbaek <seulkeebaek@gmail.com>
2026-07-30 22:05:13 +0900

    pin Jaune tracing deletion

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.32.1"
 
 require jaune from git
-  "https://github.com/skbaek/jaune.git" @ "691fb751cf57de58a10e46601d58bfb465b97f6b"
+  "https://github.com/skbaek/jaune.git" @ "c9808a575bb97491f64b178630e5616c7cee5350"
 
 @[default_target]
 lean_lib «Blanc» where
