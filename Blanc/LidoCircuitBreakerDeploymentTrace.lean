afa7c4629d2ca68b9744a91e7364f8c2b1e9d205
skbaek <seulkeebaek@gmail.com>
2026-08-24 02:54:18 +0900

    Compose official constructor effect body

--- Blanc/LidoCircuitBreakerDeploymentTrace.lean
+++ Blanc/LidoCircuitBreakerDeploymentTrace.lean
@@ -2475,6 +2475,50 @@ private theorem officialConstructorInitializedLogLine_runCompiled
   all_goals try omega
   convert hlog using 1 <;> omega
 
+private theorem officialConstructorEffectBody_runCompiled
+    {fs : List Func} {sevm : Sevm} {base : Devm} {G : Nat}
+    (hcode : sevm.code.toList = officialFullCreateInput)
+    (hpauseCold : (sevm.currentTarget, pauseDurationSlot) ∉
+      (officialConstructorPauseLoggedBase sevm base).accessedStorageKeys)
+    (hpauseOriginal : getOrigStorVal sevm sevm.currentTarget
+      pauseDurationSlot = 0)
+    (hpauseCurrent : (officialConstructorPauseLoggedBase sevm base).getStorVal
+      sevm.currentTarget pauseDurationSlot = 0)
+    (hheartbeatCold : (sevm.currentTarget, heartbeatIntervalSlot) ∉
+      (officialConstructorHeartbeatLoggedBase sevm base).accessedStorageKeys)
+    (hheartbeatOriginal : getOrigStorVal sevm sevm.currentTarget
+      heartbeatIntervalSlot = 0)
+    (hheartbeatCurrent :
+      (officialConstructorHeartbeatLoggedBase sevm base).getStorVal
+        sevm.currentTarget heartbeatIntervalSlot = 0)
+    (hstatic : sevm.isStatic = false) :
+    Func.RunCompiled fs sevm
+      (base.setMach
+        ⟨[(224 : B256), (616 : B256), (4282 : B256)],
+          officialConstructorDecodedMemory, G + 49961⟩)
+      officialConstructorEffectBody
+      (officialConstructorPost sevm base G) := by
+  have hconfiguration := officialConstructorConfigurationSuffix_runCompiled
+    (fs := fs) (G := G) hpauseCold hpauseOriginal hpauseCurrent
+    hheartbeatCold hheartbeatOriginal hheartbeatCurrent hstatic
+  have hinitialized := officialConstructorInitializedLogLine_runCompiled
+    hstatic hconfiguration
+  have hcopy := officialConstructorCopyPatch_runCompiled hcode hinitialized
+  unfold officialConstructorConfigurationSuffix
+    officialConstructorHeartbeatSuffix officialConstructorEventScratch at hcopy
+  unfold officialConstructorEffectBody
+  have hstart :
+      base.setMach
+          ⟨[(224 : B256), (616 : B256), (4282 : B256)],
+            officialConstructorDecodedMemory,
+            G + 46813 + 2163 + 985⟩ =
+        base.setMach
+          ⟨[(224 : B256), (616 : B256), (4282 : B256)],
+            officialConstructorDecodedMemory, G + 49961⟩ := by
+    congr
+  rw [← hstart]
+  exact hcopy
+
 end LidoCircuitBreaker
 
 end Blanc
